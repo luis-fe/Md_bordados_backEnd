0eb436b0adaf6d610ac6439cb09004794fac0446
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-02 11:20:18 -0300

    mudando a chamada do arquivo app.py/3 ajustando a chamada no bluePrint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import os
 from src.routes import routes_blueprint
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8080))
 
 app.register_blueprint(routes_blueprint)
 #Aqui registo todas as rotas , url's DO PROJETO, para acessar bastar ir na pasta "routes",
