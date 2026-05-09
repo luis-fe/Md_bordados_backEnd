c9223c9966ebe8a322115ed5ac73a3e493081f69
luis-fe <luis_engp@hotmail.com>
2026-05-09 16:30:51 -0300

    atualizando

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # app.py
 
 from flask import Flask
-from .src.routes import init_routes  # Importa a função do __init__.py da pasta routes
+from src.routes import init_routes  # <-- Sem o ponto inicial!
 
 app = Flask(__name__)
 
 # Chama a função que registra todas as rotas de uma vez só
 init_routes(app)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0') # O host='0.0.0.0' é importante para o Docker!
